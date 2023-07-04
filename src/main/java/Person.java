e4f8891607c5cad6fd053be788058e998ed1efbd
Ivan Ilin <ilyin1997@icloud.com>
2023-07-04 22:04:27 +0300

    add project

---
+++ src/main/java/Person.java
@@ -0,0 +1,99 @@
+import java.util.Objects;
+import java.util.OptionalInt;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    protected OptionalInt age;
+    protected String address;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, OptionalInt age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public boolean hasAge(){
+        return this.age.isPresent();
+    }
+
+    public boolean hasAddress(){
+        return !this.address.isEmpty();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public OptionalInt getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void happyBirthday (){
+        this.age = OptionalInt.of(this.age.getAsInt() + 1);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age.getAsInt() +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (!Objects.equals(name, person.name)) return false;
+        if (!Objects.equals(surname, person.surname)) return false;
+        if (!Objects.equals(age, person.age)) return false;
+        return Objects.equals(address, person.address);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (surname != null ? surname.hashCode() : 0);
+        result = 31 * result + (age != null ? age.hashCode() : 0);
+        result = 31 * result + (address != null ? address.hashCode() : 0);
+        return result;
+    }
+
+    public boolean validatePerson(){
+        if(this.name == null || this.surname == null){
+            throw new IllegalStateException("Укажите обязательные данные (имя, фамилия)");
+        }
+        return true;
+    }
+
+    public PersonBuilder newChildBuilder(){
+        PersonBuilder personBuilder = new PersonBuilder();
+        personBuilder.setSurname(this.surname);
+        personBuilder.setAddress(this.address);
+        personBuilder.setAge(0);
+        return  personBuilder;
+    }
+}
---
+++ src/main/java/PersonBuilder.java
@@ -0,0 +1,45 @@
+import java.util.OptionalInt;
+
+public class PersonBuilder {
+    protected  String name;
+    protected  String surname;
+    protected OptionalInt age;
+    protected String address;
+
+    public PersonBuilder setName(String name){
+        this.name = name;
+        return this;
+    }
+
+    public PersonBuilder setSurname(String surname){
+        this.surname = surname;
+        return this;
+    }
+
+    public PersonBuilder setAge(int age){
+        if(age<0){
+            throw new IllegalArgumentException("Некорректный возраст: " + age);
+        }
+        this.age = OptionalInt.of(age);
+        return this;
+    }
+
+    public PersonBuilder setAddress(String address){
+        this.address = address;
+        return this;
+    }
+
+    public Person build(){
+        Person person;
+        if(this.age != null){
+            person = new Person(name, surname, age);
+        }else {
+            person = new Person(name, surname);
+        }
+        if (address != null){
+            person.setAddress(address);
+        }
+        person.validatePerson();
+        return person;
+    }
+}
